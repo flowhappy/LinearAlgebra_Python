60ba8fc758c8d5f9d9a55b71aec519332f851ad3
Lloyd <lyd610225@icloud.com>
2023-08-16 11:38:55 +0800

    add mpl.py

---
+++ py_LA/Mpl.py
@@ -0,0 +1,52 @@
+from py_LA.Matrix import Matrix
+import math
+
+
+class MplMtr:
+    """
+        矩阵变换类
+        使用这个类创建的矩阵，自带变换方法
+    """
+
+    def __init__(self, lst2d):
+        self._values = Matrix(lst2d).T()
+
+    def __repr__(self):
+        return "Mpl{}".format(self._values)
+
+    __str__ = __repr__
+
+    @staticmethod
+    def get_points(mtr):
+        return [mtr.col_vector(i)[0] for i in range(mtr.col_num())], \
+            [mtr.col_vector(i)[1] for i in range(mtr.col_num())]
+
+    def transform_mtr(self, lst2d):
+        """
+        根据传入的变换矩阵，返回变化后的矩阵
+        """
+        return self.get_points(Matrix(lst2d).dot_mul(self._values))
+
+    def x_axis_flip(self):
+        # 沿X轴翻转 的变换矩阵
+        return self.transform_mtr([[1, 0], [0, -1]])
+
+    def y_axis_flip(self):
+        # 沿Y轴翻转 的变换矩阵
+        return self.transform_mtr([[-1, 0], [0, 1]])
+
+    def o_flip(self):
+        # 关于原点对称 的变换矩阵
+        return self.transform_mtr([[-1, 0], [0, -1]])
+
+    def x_shear(self, k):
+        # 沿X轴错切 的变换矩阵
+        return self.transform_mtr([[1, k], [0, 1]])
+
+    def y_shear(self, k):
+        # 沿Y轴错切 的变换矩阵
+        return self.transform_mtr([[1, 0], [k, 1]])
+
+    def rotate(self, theta):
+        # 旋转
+        return self.transform_mtr([[math.cos(theta), math.sin(theta)], [-math.sin(theta), math.cos(theta)]])
---
+++ test_mpl.py
@@ -0,0 +1,28 @@
+import math
+from py_LA.Mpl import MplMtr
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+
+    points = [
+        [0, 0], [0, 5], [3, 5], [3, 4], [1, 4],
+        [1, 3], [2, 3], [2, 2], [1, 2], [1, 0]
+    ]
+    x = [points[i][0] for i in range(len(points))]
+    y = [points[i][1] for i in range(len(points))]
+    # 调整绘制窗口的大小
+    plt.figure(figsize=(5, 5))
+    # X轴从-10到10
+    plt.xlim(-10, 10)
+    # Y轴从-10到10
+    plt.ylim(-10, 10)
+    plt.plot(x, y)
+
+    """
+    和作业相关
+    """
+    mpl = MplMtr(points)
+    x, y = mpl.rotate(math.pi / 3)
+    plt.plot(x, y)
+
+    plt.show()
