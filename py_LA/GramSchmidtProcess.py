c907c0658877ca6396777b1140220d2847a89506
flowhappy <lyd610225@icloud.com>
2023-08-21 22:28:01 +0800

    add qr

--- py_LA/GramSchmidtProcess.py
+++ py_LA/GramSchmidtProcess.py
@@ -14,3 +14,14 @@ def gram_schmidt_process(basis):
             p = p - basis[i].dot(r) / r.dot(r) * r
         res.append(p)
     return res
+
+
+def qr(A):
+    assert A.row_num() == A.col_num(), "A must be square"
+
+    basis = [A.col_vector(i) for i in range(A.col_num())];
+    P = gram_schmidt_process(basis)
+    Q = Matrix([v / v.norm() for v in P]).T()
+    R = Q.T().dot(A)
+
+    return Q, R
