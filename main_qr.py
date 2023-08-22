ae6b5c8539128ca800221f4d6269e7a5a2030ef2
flowhappy <lyd610225@icloud.com>
2023-08-22 21:36:29 +0800

    add qr

--- main_qr.py
+++ main_qr.py
@@ -1,18 +1,15 @@
 from py_LA.Matrix import Matrix
 from py_LA.GramSchmidtProcess import qr
 
-
 if __name__ == "__main__":
-
     A1 = Matrix([[1, 1, 2],
-                [1, 1, 0],
-                [1, 0, 0]])
+                 [1, 1, 0],
+                 [1, 0, 0]])
     Q1, R1 = qr(A1)
     print(Q1)
     print(R1)
     print(Q1.dot(R1))
     print()
-
 
     A2 = Matrix([[2, -1, -1],
                  [2, 0, 2],
